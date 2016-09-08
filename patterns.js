e417f9111c2c038d57f2e428cd0fc9e1767211ab
Mamatha Siripi <msiripi@osius.com>
2016-09-08 16:54:10 +0530

    sep-7 files

---
+++ patterns.js
@@ -0,0 +1,67 @@
+﻿//factory patterns
+
+var facrotyparent = function (name,age,city) {
+    var test = {};
+
+    test.name = name;
+    test.age = age;
+    test.city = city;
+
+    test.factorychild = function () {
+      
+     // document.write(test.name + "," + test.age + "," + test.city);
+
+    }
+    return test;
+}
+
+var f = facrotyparent('mamtha', 24, 'kurnool');
+var f1 = facrotyparent('mamtha1', 24, 'kurnool1')
+f.factorychild();
+f1.factorychild();
+
+//constructor patterns
+
+function constructorparent(name,age,city) {
+    this.name = name;
+    this.age = age;
+    this.city = city;
+
+    this.constructorchild = function () {
+     //   document.write(this.name + "," + this.age + "," + this.age);
+    }
+    return this;
+}
+
+var c = new constructorparent('venkat', 24, 'vizag')
+var c1 = new constructorparent('venkat1', 24, 'vizag1');
+c.constructorchild();
+c1.constructorchild();
+
+//prototype patterns
+
+
+
+
+
+
+//denamic prototypattern
+
+function denamicprototypeparent(name, age, city) {
+    this.name = name;
+    this.age = age;
+    this.city = city;
+
+    if (typeof printchild !== 'function') {
+        denamicprototypeparent.prototype.printchild = function () {
+            document.write(this.name + " ," + this.age + "," + this.city)
+        }
+    }
+
+}
+
+
+var d = new denamicprototypeparent("bekkem", 24, 'nandyala');
+var d1 = new denamicprototypeparent("bekkem1", 24, 'nadyala1');
+d.printchild();
+d1.printchild()
---
+++ setTimeOut.js
@@ -0,0 +1,105 @@
+﻿
+var addTo=function(passed) {
+  
+  
+   function add() {
+        return passed + 8;
+    }
+  return add;
+
+}
+
+var d = addTo(5)();
+document.write(d);
+
+
+
+
+function greet(message) {
+    console.log(message);
+}
+
+function greeter(name, age) {
+ 
+    return name + ", who is " + age + " years old, says hi!";
+}
+
+// Generate the message
+var message =greeter("Bob", 47);
+
+// Pass it explicitly to greet
+greet(message)
+
+
+function greeter(name, age) {
+    var message = name + ", who is " + age + " years old, says hi!";
+
+    return  function greet() {
+       console.log(message);
+    };
+  
+}
+
+// Generate the closure
+var bobGreeter =new greeter("mamatha", 24);
+
+// Use the closure
+bobGreeter();
+
+
+
+
+
+function setAlarm(message1, timeout) {
+
+    function handle() {
+        console.log(message1);
+    }
+
+
+    setTimeout(handle, timeout);
+}
+
+setAlarm("Wake UP!", 100);
+
+
+
+function mainfun() {
+    this.mainmessage="example about prototype functions"
+}
+
+mainfun.prototype.firstfun = function () {
+  //  document.write(this.mainmessage)
+}
+
+mainfun.prototype.secondfun = function () {
+    var self = this;
+    setTimeout(function () {
+        document.write(self.mainmessage)
+    },1000)
+}
+
+var k = new mainfun();
+k.firstfun();
+k.secondfun();
+
+
+function mainfun1() {
+    var message3 = "this is close with out using namespaces";
+
+    return {
+        firstfun1: function () {
+            alert(message3)
+        },
+
+
+        secondfun2: function () {
+            setTimeout(function () {
+                alert(message3)
+            },100)
+        }
+    }
+}
+var n = new mainfun1();
+n.firstfun1();
+n.secondfun2();
