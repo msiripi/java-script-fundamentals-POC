c3481569946598045083203eb5fd27100e7c35be
Mamatha Siripi <msiripi@osius.com>
2016-09-08 17:06:24 +0530

    namespace example

---
+++ namespace/teamA.js
@@ -0,0 +1,15 @@
+﻿var pargimtech = pargimtech || {};
+pargimtech.teamA = pargimtech.teamA || {};
+
+
+pargimtech.teamA.customer = function (fname, lname) {
+    this.Fname = fname;
+    this.Lname = lname;
+    this.getfun = function () {
+        return this.Fname + this.Lname;
+    }
+    return this;
+}
+
+ console.log(pargimtech.teamA.customer('arjun', 'karthik').getfun());
+
---
+++ namespace/teamB.js
@@ -0,0 +1,14 @@
+﻿var pargimtech = pargimtech || {};
+pargimtech.teamB = pargimtech.teamB || {};
+
+pargimtech.teamB.customer = function (fname,lname,age) {
+    this.Fname = fname;
+    this.Lname = lname;
+    this.Age = age;
+
+    this.getdata = function () {
+        return this.Fname + this.Lname + this.Age;
+    }
+
+    return this;
+}
